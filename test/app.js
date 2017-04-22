3e8a2b33e166b2ddb19168dee7a2dc7a96745fda
Chandan Shankarappa <shankarappa.c@husky.neu.edu>
2017-04-21 22:03:59 -0400

    connection string

--- test/app.js
+++ test/app.js
@@ -4,15 +4,15 @@ module.exports = function(app)
     app.post("/api/test", createMessage);
     app.delete("/api/test/:id", deleteMessage);
 
-    var connectionString = 'mongodb://127.0.0.1:27017/test';
+    var connectionString = 'cshankarappa:admin@ds111549.mlab.com:11549/sandbox';
 
-    if(process.env.MLAB_USERNAME) {
-        connectionString = process.env.MLAB_USERNAME + ":" +
-            process.env.MLAB_PASSWORD + "@" +
-            process.env.MLAB_HOST + ':' +
-            process.env.MLAB_PORT + '/' +
-            process.env.MLAB_APP_NAME;
-    }
+    // if(process.env.MLAB_USERNAME) {
+    //     connectionString = process.env.MLAB_USERNAME + ":" +
+    //         process.env.MLAB_PASSWORD + "@" +
+    //         process.env.MLAB_HOST + ':' +
+    //         process.env.MLAB_PORT + '/' +
+    //         process.env.MLAB_APP_NAME;
+    // }
 
     var mongoose = require("mongoose");
     mongoose.connect(connectionString);
